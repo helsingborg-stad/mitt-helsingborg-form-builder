3fd90aa08aa292e70cc93c85a54c8fc49e52ca11
Jacob Winding <jacobkwinding@gmail.com>
2020-09-22 10:38:51 +0200

    Fix order of step input fields to match what is shown

--- src/components/specific/Steps/StepDataField.tsx
+++ src/components/specific/Steps/StepDataField.tsx
@@ -4,9 +4,9 @@ import MultipleInputField from '../../general/MultipleInputField';
 import { InputFieldPropType } from '../../../types/PropTypes';
 
 const stepFields: FieldDescriptor[] = [
-  { name: 'title', type: 'text', initialValue: '', label: 'Title' },
-  { name: 'description', type: 'text', initialValue: '', label: 'Description' },
-  { name: 'group', type: 'text', initialValue: '', label: 'Group' },
+  { name: 'title', type: 'text', initialValue: '', label: 'Title (big headline)' },
+  { name: 'group', type: 'text', initialValue: '', label: 'Group (small text under icon) ' },
+  { name: 'description', type: 'text', initialValue: '', label: 'Description (small descriptive text below title)' },
   { name: 'id', type: 'text', initialValue: '', label: 'Id' },
 ];
 
