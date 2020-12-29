5c6ba70f7c3031ce207ee1372f7d429284bd6b78
Jacob Winding <jacobkwinding@gmail.com>
2020-12-29 14:47:12 +0100

    nav-button-preview: update prop name so that the colors work correctly in the preview

--- src/preview/components/NavigationButtonGroup/NavigationButtonGroup.tsx
+++ src/preview/components/NavigationButtonGroup/NavigationButtonGroup.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from '../Button/Button';
 import Text from '../Text/Text';
-import { getValidColorSchema } from '../../styles/themeHelpers';
+import { getValidColorSchema, PrimaryColor } from '../../styles/themeHelpers';
 
 const ScrollView = styled.div<{ horizontal?: boolean }>`
   display: inline-flex;
@@ -12,16 +12,21 @@ const ScrollView = styled.div<{ horizontal?: boolean }>`
 `;
 
 interface Props {
-  buttons?: { text: string; iconName?: string; color?: string }[];
+  buttons?: { text: string; iconName?: string; colorSchema?: PrimaryColor }[];
   horizontal?: boolean;
+  colorSchema: PrimaryColor;
 }
 
-const NavigationButtonGroup: React.FC<Props> = ({ buttons, horizontal }) => (
+const NavigationButtonGroup: React.FC<Props> = ({ buttons, horizontal, colorSchema }) => (
   <ScrollView horizontal={horizontal}>
     {buttons &&
       Array.isArray(buttons) &&
       buttons.map((button, index) => (
-        <Button key={`nav-button-${index}`} colorSchema={getValidColorSchema(button.color || '')} variant="outlined">
+        <Button
+          key={`nav-button-${index}`}
+          colorSchema={getValidColorSchema(button.colorSchema || colorSchema)}
+          variant="outlined"
+        >
           <Text>{button.text}</Text>
         </Button>
       ))}
