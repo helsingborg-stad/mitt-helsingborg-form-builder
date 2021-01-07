c7297d76344273fe7586bb006afee57114ba5570
Jacob Winding <jacobkwinding@gmail.com>
2021-01-07 11:48:20 +0100

    feat(action-conditions): add option to add conditions to actions

--- src/components/specific/Steps/ActionField.tsx
+++ src/components/specific/Steps/ActionField.tsx
@@ -32,10 +32,36 @@ const actionFields: FieldDescriptor[] = [
       { value: 'neutral', name: 'Neutral (gray)' },
     ],
   },
+  {
+    name: 'hasCondition',
+    type: 'checkbox',
+    initialValue: '',
+    label: 'Add condition to the action?',
+    helpText: 'Make the action dependent on some condition, dependant on other field values.',
+  },
+];
+
+const conditionInput: FieldDescriptor[] = [
+  {
+    name: 'conditionalOn',
+    type: 'text',
+    initialValue: '',
+    label: 'Conditional on',
+    helpText: `Make the action depend on values of another field. Most basic usage is to put a fieldId here, then the action is only allowed if that field is filled. Multiple fieldIds can also be entered, combining them with boolean logic operators ! (not), &&(and), || (or). For example 'field1 || field2' means that either field1 or field2 needs to be filled, while 'field1 && field2' means that both fields needs to have values, and so on. The order of operations is first !, then &&, finally ||. 
+    
+    For lists and repeaters, empty means "no values", while filled means "at least one non-empty value". `,
+  },
 ];
 
 const ActionField: React.FC<InputFieldPropType> = (props) => {
-  return <MultipleInputField fields={actionFields} {...props} />;
+  const { value, name } = props;
+  const hasCondition: boolean = value?.hasCondition && value.hasCondition === true;
+  return (
+    <>
+      <MultipleInputField fields={actionFields} {...props} />
+      {hasCondition && <MultipleInputField fields={conditionInput} {...props} />}
+    </>
+  );
 };
 
 export default ActionField;
