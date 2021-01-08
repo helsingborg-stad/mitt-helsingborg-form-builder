1fb8f39de1a160f0b380c025b43a00b78c5b1192
Jacob Winding <jacobkwinding@gmail.com>
2021-01-08 16:18:49 +0100

    add NavigationButton to preview, and add icons to navigation buttons in the preview

--- src/preview/components/NavigationButtonGroup/NavigationButtonGroup.tsx
+++ src/preview/components/NavigationButtonGroup/NavigationButtonGroup.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Button from '../Button/Button';
 import Text from '../Text/Text';
 import { getValidColorSchema, PrimaryColor } from '../../styles/themeHelpers';
+import NavigationButton from './NavigationButton';
 
 const ScrollView = styled.div<{ horizontal?: boolean }>`
   display: inline-flex;
@@ -22,13 +23,12 @@ const NavigationButtonGroup: React.FC<Props> = ({ buttons, horizontal, colorSche
     {buttons &&
       Array.isArray(buttons) &&
       buttons.map((button, index) => (
-        <Button
+        <NavigationButton
           key={`nav-button-${index}`}
           colorSchema={getValidColorSchema(button.colorSchema || colorSchema)}
-          variant="outlined"
-        >
-          <Text>{button.text}</Text>
-        </Button>
+          text={button.text}
+          iconName={button.iconName}
+        />
       ))}
   </ScrollView>
 );
