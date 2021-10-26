29b169d8f90bf5aa933b80a67389b67475327ec3
Sven65 <Sven65@users.noreply.github.com>
2021-10-26 14:36:59 +0200

    Add select dropdown to repeater fields

--- src/components/specific/Questions/InputTypes/RepeaterInputField.tsx
+++ src/components/specific/Questions/InputTypes/RepeaterInputField.tsx
@@ -4,6 +4,7 @@ import MultipleInputField from '../../../general/MultipleInputField';
 import { InputFieldPropType } from '../../../../types/PropTypes';
 import QuestionTypeSelect from '../QuestionTypeSelect';
 import { ValidationFieldTypes } from '../ValidationRules';
+import SelectChoice from './SelectChoice';
 
 const fields: FieldDescriptor[] = [
   { name: 'title', type: 'text', initialValue: '', label: 'Title' },
@@ -25,13 +26,22 @@ const typeChoices: {
   { selectValue: 'number', displayName: 'Number', inputType: 'number', validationType: 'number' },
   { selectValue: 'date', displayName: 'Date', inputType: 'date', validationType: 'date' },
   { selectValue: 'checkbox', displayName: 'Checkbox', inputType: 'checkbox', validationType: 'checkbox' },
+  { selectValue: 'select', displayName: 'Select (dropdown)', inputType: 'select', validationType: 'select' },
 ];
 
+const extraInputs: Partial<Record<InputType, FieldDescriptor[]>> = {
+  select: [{ name: 'items', type: 'array', initialValue: '', label: 'Choices', inputField: SelectChoice }],
+};
+
 const RepeaterInputField: React.FC<InputFieldPropType> = (props: InputFieldPropType) => {
+  const { value } = props;
+  const extraInput = Object.keys(extraInputs).includes(value.type) && extraInputs[value.type as InputType];
+
   return (
     <>
       <MultipleInputField fields={fields} {...props} />
       <QuestionTypeSelect name={props.name} value={props.value} label="Input field type" choices={typeChoices} />
+      {extraInput && <MultipleInputField fields={extraInput} {...props} />}
     </>
   );
 };
