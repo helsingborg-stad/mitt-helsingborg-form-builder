815988206eff60fc22306dc0f01695f37b2a7636
Petter Andersson <petter.andersson@helsingborg.se>
2022-03-03 11:48:47 +0100

    feat: lägg till sorterings property till kategorier
    När en summarylist används, inordnas fält under kategorier. För att kunna visa repeaters
    i en viss önskad ordning så behöver man tillföra ett fält som indikerar vilket fält som skall
    användas som sorteringsnyckel. Appen kommer sedemera att använda detta värdet för att utföra sorteringen.

--- src/components/specific/Questions/InputTypes/SummaryList/CategoryField.tsx
+++ src/components/specific/Questions/InputTypes/SummaryList/CategoryField.tsx
@@ -6,6 +6,7 @@ import { InputFieldPropType } from '../../../../../types/PropTypes';
 const fields: FieldDescriptor[] = [
   { name: 'category', type: 'text', initialValue: '', label: 'Category id' },
   { name: 'description', type: 'text', initialValue: '', label: 'Display name' },
+  { name: 'sortField', type: 'text', initialValue: '', label: 'Sort field' },
 ];
 
 const CategoryField: React.FC<InputFieldPropType> = (props: InputFieldPropType) => {
