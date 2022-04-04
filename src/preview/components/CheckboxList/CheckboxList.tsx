88f2b8cbb05adc4f0fb0259744881a20aff17920
Sven65 <Sven65@users.noreply.github.com>
2022-04-04 10:16:32 +0200

    Refactor according to PR

--- src/preview/components/CheckboxList/CheckboxList.tsx
+++ src/preview/components/CheckboxList/CheckboxList.tsx
@@ -3,12 +3,6 @@ import PropTypes from 'prop-types';
 import Text from '../Text/Text';
 import { Checkbox, FormGroup, FormControlLabel } from '@material-ui/core';
 
-interface Props {
-  text?: string;
-  color?: 'blue' | 'green' | 'purple' | 'red';
-  size?: 'small' | 'medium' | 'large';
-}
-
 interface CheckboxListChoice {
   displayText: string;
   tags?: string[];
@@ -19,39 +13,17 @@ interface CheckboxListProps {
   choices?: CheckboxListChoice[];
 }
 
-type ComponentProps = Props & CheckboxListProps;
-
 /** A component with a checkbox next to a descriptive text, and possibly a help button */
-const CheckboxList: React.FC<ComponentProps> = (props) => {
+const CheckboxList: React.FC<CheckboxListProps> = ({ choices }) => {
   return (
     <FormGroup row>
-      {props.choices?.map((choice) => (
+      {choices?.map((choice) => (
         <>
           <FormControlLabel control={<Checkbox />} label={<Text>{choice.displayText}</Text>} />
         </>
       ))}
     </FormGroup>
   );
-};
-
-CheckboxList.propTypes = {
-  /**
-   * The text to show at the side of the checkbox.
-   */
-  text: PropTypes.string,
-  /**
-   * sets the color theme.
-   */
-  color: PropTypes.oneOf(['blue', 'green', 'red', 'purple']),
-  /**
-   * One of small, medium, large
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-};
-
-CheckboxList.defaultProps = {
-  color: 'blue',
-  size: 'small',
 };
 
 export default CheckboxList;
