c3f3b546a1cab603887748077208d7eed981e5ad
Karl Mösenbacher <karlmosenbacher@MBPsomtlhorKarl.lan>
2022-07-05 15:20:03 +0200

    fix(formbuilderhelpers.test.ts): small fixes in test file

--- src/components/specific/test/FormBuilderHelpers.test.ts
+++ src/components/specific/test/FormBuilderHelpers.test.ts
@@ -1,25 +1,6 @@
 import { computeMatrix, getStepstructureIds } from '../FormBuilderHelpers';
 
-const stepStructure = [
-  {
-    children: [],
-    group: '2f200411-9ea6-44a6-b207-7ccdaa5606ba',
-    id: '5d29a0be-6b6a-444b-8f39-23dfe78b42a8',
-    text: 'Fråga 1',
-  },
-  {
-    children: [],
-    group: '808963dd-8a82-4e7f-9bd7-8449a7b9650a',
-    id: '2fd23656-7004-42e5-a68c-828659da574b',
-    text: 'Fråga 2',
-  },
-  {
-    children: [],
-    group: '495913a7-b365-4e2a-8552-3a11036d345e',
-    id: '652d65f2-1fd5-42a9-9442-ea05d8142ac2',
-    text: 'Fråga 3',
-  },
-];
+import { ListItem } from '../../../types/FormTypes';
 
 describe('computeMatrix', () => {
   const expectedMatrix = [
@@ -29,13 +10,34 @@ describe('computeMatrix', () => {
   ];
 
   it('returns the death matrix', () => {
+    const stepStructure = [
+      {
+        children: [],
+        group: '2f200411-9ea6-44a6-b207-7ccdaa5606ba',
+        id: '5d29a0be-6b6a-444b-8f39-23dfe78b42a8',
+        text: 'Fråga 1',
+      },
+      {
+        children: [],
+        group: '808963dd-8a82-4e7f-9bd7-8449a7b9650a',
+        id: '2fd23656-7004-42e5-a68c-828659da574b',
+        text: 'Fråga 2',
+      },
+      {
+        children: [],
+        group: '495913a7-b365-4e2a-8552-3a11036d345e',
+        id: '652d65f2-1fd5-42a9-9442-ea05d8142ac2',
+        text: 'Fråga 3',
+      },
+    ];
+
     const result = computeMatrix(stepStructure);
 
     expect(result).toEqual(expectedMatrix);
   });
 
-  it('returns the death matrix with changed stepstructure', () => {
-    const stepst = [
+  it('returns the death matrix with changed stepStructure order', () => {
+    const stepStructure = [
       {
         children: [],
         group: '808963dd-8a82-4e7f-9bd7-8449a7b9650a',
@@ -56,7 +58,7 @@ describe('computeMatrix', () => {
       },
     ];
 
-    const result = computeMatrix(stepst);
+    const result = computeMatrix(stepStructure);
 
     expect(result).toEqual(expectedMatrix);
   });
@@ -64,7 +66,7 @@ describe('computeMatrix', () => {
 
 describe('getStepstructureIds', () => {
   it('flattens ids from stepstructure', () => {
-    const stepst = [
+    const stepStructure = ([
       {
         children: [],
         id: '1',
@@ -77,14 +79,14 @@ describe('getStepstructureIds', () => {
         children: [],
         id: '3',
       },
-    ];
-    const result = getStepstructureIds(stepst);
+    ] as unknown) as ListItem[];
+    const result = getStepstructureIds(stepStructure);
 
     expect(result).toEqual(['1', '2', '3']);
   });
 
   it('flattens ids from stepstructure with children', () => {
-    const stepst = [
+    const stepStructure = ([
       {
         children: [
           {
@@ -117,9 +119,8 @@ describe('getStepstructureIds', () => {
         ],
         id: '5',
       },
-    ];
-    const result = getStepstructureIds(stepst);
-    console.log('RESULT: ', result);
+    ] as unknown) as ListItem[];
+    const result = getStepstructureIds(stepStructure);
 
     expect(result).toEqual(['1', '2', '3', '4', '5', '6', '7']);
   });
